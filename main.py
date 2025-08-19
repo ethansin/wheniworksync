9e6dbdd90b83f974a5bfcafb263d02078ba8e17a
ethansin <elsin@ucsc.edu>
2025-08-19 15:20:55 -0700

    Add some verbosity to when the script starts and how long it has been running.

--- main.py
+++ main.py
@@ -13,8 +13,16 @@ def main():
     create_events(events)
 
 if __name__ == "__main__":
+
+    print("Starting When I Work Calendar Sync...")
     schedule.every().day.at("00:00").do(main)
 
+    count = 0
+    hours = 0
     while True:
+        if count % 60 == 0:
+            hours += 1
+            print(f"Calendar Sync has been running for {hours} hours.")
         schedule.run_pending()
         time.sleep(60)
+        count += 1
